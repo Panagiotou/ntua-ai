6a216be0e67236689e8424a5cc9d7360d007d9f3
Marios Papachristou <papachristoumarios@gmail.com>
2018-12-06 02:35:42 +0200

    Comparable Estimator

--- lab1/src/Estimator.java
+++ lab1/src/Estimator.java
@@ -1,18 +1,25 @@
 
-public class Estimator {
+public class Estimator implements Comparable {
 	public Node from;
 	public Node to;
 	public double actual_distance;
 	public double heuristic_estimate;
 	
-	public Estimator(Node u, Node v, double g, double h) {
+	public Estimator(Node u, double g, double h) {
 		from = u;
-		to = v;
 		actual_distance = g;
 		heuristic_estimate = h;
 	}
 	
 	public double getCost() {
 		return actual_distance + heuristic_estimate;
+	}
+	
+	@Override
+	public int compareTo(Object arg0) {
+		if (arg0.getClass() != getClass()) return -1;
+		Estimator other = (Estimator) arg0;
+		if (getCost() < other.getCost()) return -1;
+		else return 1;
 	}
 }
