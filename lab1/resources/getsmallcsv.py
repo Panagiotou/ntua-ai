49e4bc039656deb4df025e6c482326192f8e3928
Panagiotou <manopilipana97@gmail.com>
2018-12-06 20:07:50 +0200

    Added graph creation section to the report

--- lab1/resources/getsmallcsv.py
+++
@@ -1,8 +0,0 @@
-import csv
-i = 0
-with open("newnodes.csv", "w") as f:
-    for line in open("nodes.csv", encoding= 'latin-1'):
-        f.write(line)
-        i += 1;
-        if(i == 1000):
-            break;
